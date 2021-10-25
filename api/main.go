33957f7eed0d974d4c5ff0d1eea8fcaa4bda2de8
UnplugCharger <isaacbyron@gmail.com>
2021-10-25 22:38:44 +0300

    Jwt server complete

--- api/main.go
+++ api/main.go
@@ -14,17 +14,44 @@ func homePage(w http.ResponseWriter , r *http.Request)  {
 	fmt.Fprintf(w,"This is a very secrete info")
 }
 
-func isAuthorised(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
-	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request){
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		if r.Header["Token"] != nil {
-			fmt.Println("Mahinya")
+		token , err :=	jwt.Parse(r.Header["Token"][0],func(token *jwt.Token)(interface{},error){
+
+
+			if _, ok :=	token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil , fmt.Errorf(("Invalid signing method"))
+			}   
+			    aud := "billing.jwtgo.io"
+				checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud , false)
+				if !checkAudience {
+					return nil , fmt.Errorf(("Invalid aud"))
+				}
+				iss := "jwtgo.io"
+				checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss , false)
+				if !checkIss{
+					return nil ,fmt.Errorf(("invalid iss"))
+				}
+
+				return MySigningKey , nil
+			})
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+			if token.Valid{
+				endpoint(w,r)
+			}
+			
+		}else {
+			fmt.Fprintf(w, "No auth tokens provided ")
 		}
-		fmt.Println("Byron")
+		
 	})
+}
 
-
-func handleRequests ()  {
-	http.Handle("/",isAuthorised(homePage))
+ func handleRequests(){
+	http.Handle("/",isAuthorized(homePage))
 	log.Fatal(http.ListenAndServe(":9001", nil))
 }
 
